de35e0dcfd395292af00209c771645f465a5f327
AlinurAbdyiskakov <75415137+AlinurAbdyiskakov@users.noreply.github.com>
2021-01-12 15:31:10 +0600

    the change

--- scripts.js
+++ scripts.js
@@ -13,6 +13,9 @@ alert(firstName);
 alert(lastName);
 alert(placeOfBirth);
 
-console.log(firstName);
-console.log(lastName);
-console.log(placeOfBirth);
+// console.log(firstName);
+// console.log(lastName);
+// console.log(placeOfBirth);
+
+let message = "My name is " + firstName + ", my last name is " + lastName +",I,m from " + placeOf Birth + "";
+console.log(message);
