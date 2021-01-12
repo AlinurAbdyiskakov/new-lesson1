1cecad800631d3766170fde9a8e3e11a5e4043a0
AlinurAbdyiskakov <75415137+AlinurAbdyiskakov@users.noreply.github.com>
2021-01-12 15:03:35 +0600

    the change add new alert

--- scripts.js
+++ scripts.js
@@ -2,8 +2,12 @@ let firstName = "Alinur";
 let lastName = "Abdyiskakov";
 let placeOfBirth ="Boz-Beshik";
 
-alert(firstName);
+// alert(firstName);
 // alert(lastName);
 // alert(placeOfBirth);
 firstName = "Al";
 alert(firstName);
+lastName ="abdy";
+alert(lastName);
+placeOfBirth = (Boz);
+alert(placeOfBirth);
